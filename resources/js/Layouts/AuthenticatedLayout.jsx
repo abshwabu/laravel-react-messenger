0b258640821a4781b0a7ed67031995c1b150d988
abdulaziz <abdulazizredi90@gmail.com>
2024-12-26 10:48:03 +0300

    scrolling solved

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -12,14 +12,14 @@ export default function AuthenticatedLayout({ header, children }) {
         useState(false);
 
     return (
-        <div className="min-h-screen bg-gray-100">
-            <nav className="border-b border-gray-100 bg-white">
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen dark:bg-gray-900 bg-gray-100 flex flex-col h-screen">
+            <nav className="border-b border-gray-100 bg-white dark:bg-gray-900">
+                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
                     <div className="flex h-16 justify-between">
                         <div className="flex">
                             <div className="flex shrink-0 items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo className="block h-9 w-auto fill-current dark:text-gray-100 text-gray-800" />
                                 </Link>
                             </div>
 
@@ -170,7 +170,7 @@ export default function AuthenticatedLayout({ header, children }) {
                 </header>
             )}
 
-            <main>{children}</main>
+            {children}
         </div>
     );
 }
